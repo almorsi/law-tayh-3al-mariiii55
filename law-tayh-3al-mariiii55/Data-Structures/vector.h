5fd6cb48a856fe819b95d2b3714d019aed5d6df7
Ammar Nasser <ammarnasser0012@gmail.com>
2021-11-21 01:18:25 +0200

    VECTOR IMPLEMENTAION COMPLETED

--- law-tayh-3al-mariiii55/Data-Structures/vector.h
+++ law-tayh-3al-mariiii55/Data-Structures/vector.h
@@ -1,1 +1,48 @@
-#pragma once
+#ifndef VECTOR_H
+#define VECTOR_H
+#include <cmath>
+template <typename T>
+class Vector
+{
+public:
+	Vector();
+	~Vector();
+	long getLength() const;
+	long getCapacity() const;
+	void insertEnd(const T&);
+	void insertBegin(const T&);
+	void deleteBegin();
+	void deleteEnd();
+	T getAt(long k) const;
+	void setAt(long, T);
+	T getFirst()const;
+	void setFirst(T);
+	bool isEmpty()const;
+
+private:
+	T* array;
+	long capacity;
+	long begin;
+	long end;
+	static constexpr long INSERTION_RESIZE_FACTOR = 2;
+	static constexpr double BEGIN_RATIO = 0.33;
+	static constexpr long INITIIAL_CAPACITY = 4;
+	static constexpr long INITIIAL_BEGIN = 1;
+	static constexpr long INITIIAL_END = 2;
+
+private:
+	void resize(double);
+	void setCapacity(long);
+	long getBegin()const;
+	void setBegin(long);
+	void setEnd(long);
+	long getEnd()const;
+	long getRealAddress(long) const;
+	void incrementEnd();
+	void incrementBegin();
+	void decrementBegin();
+	void decrementEnd();
+	bool isValidRealAddress(long) const;
+	bool isHasToResizeOnInsertion();
+};
+#endif
---
+++ law-tayh-3al-mariiii55/Tests/VectorTest.h
@@ -0,0 +1,41 @@
+#ifndef VECTOR_TEST_H
+#define VECTOR_TEST_H
+#include <cassert>
+
+#include "../Data-Structures/Vector.h"
+
+class VectorTest
+{
+public:
+	VectorTest();
+	~VectorTest();
+	void runAllTests();
+private:
+	void initializationTest();
+	void insertFirstTestAfterInsertingOneLast();
+	void getLengthTestWithoutInsertion();
+	void getLengthTestAfterInsertingOneItem();
+	void getLengthTestAfterInsertingTwoItems();
+	void getCapacityTestWithoutInsertion();
+	void insertBeginFiveTimesAndinsertEndFiveTimes();
+	void insert1000ItemsAtTheBegining();
+	void insertBeginTheninsertEnd100Times();
+	void insertEnd1000Times();
+	void insertOneItemAndTestGetAt0();
+	void setAtShouldThrowExecptionAfterPassingInvalidIndex();
+	void setAtShouldThrowExecptionAfterPassingValidIndexButInEmptyVector();
+	void setAtShouldNotThrowExecptionAfterPassingValidIndex();
+	void getFirstShouldThrowExecptionOnImptyVector();
+	void getFirstShouldGetTheFirstItemAfterInsertingTowToTheBeggining();
+	void setFirstShouldThrowExeceptionOnEmptyVector();
+	void setFirstShouldNotThrowExeceptionOnValidVector();
+	void InsertBeginThenDeleteBegin();
+	void insert1000ThenDelete500();
+	void deleteBeginShouldThrowExecptionIfDeletingMoreThanInserting();
+	void insertEndThenDeleteEnd();
+	void deleteEndShouldThrowExecptionOnDeletingFromEmptyVector();
+	void insertBegin1000ThenDeleteEnd500();
+	void deleteEndShouldThrowExecptionOnDeletingMoreThanInserting();
+	void deleteTheSameAmountAsInserting();
+};
+#endif
