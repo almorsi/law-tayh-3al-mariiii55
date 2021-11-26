ead5385f1715a91942b9a4d0c20ab49cf07b4fd7
Ammar Nasser <ammarnasser0012@gmail.com>
2021-11-26 20:36:24 +0200

    input Device and fille input device implemented and tested

--- law-tayh-3al-mariiii55/IO/InputDevice.h
+++ law-tayh-3al-mariiii55/IO/InputDevice.h
@@ -1,14 +1,18 @@
 #ifndef INPUTDEVICE_H
 #define INPUTDEVICE_H
+
+#include "..\Data-Structures\vector.h"
 class InputDevice
 {
 public:
 	InputDevice() {};
 	virtual ~InputDevice() {};
-	virtual void setInputSource() = 0;
-	virtual bool canParseInputFromSource() = 0;
+	virtual void setInputSource(const char*) = 0;
+	virtual bool canParseInputFromSource() const = 0;
+	virtual Vector<int>* getParsedInput() = 0;
+
+protected:
 	virtual void parseInput() = 0;
-	virtual void getParsedInput() = 0;
 };
 
 #endif // !INPUTDEVICE_H
