8161f8aa4656ec9e68fe9e9ec8c46f370c2192e6
Ammar Nasser <ammarnasser0012@gmail.com>
2021-11-26 21:43:09 +0200

    Output Device interface

---
+++ law-tayh-3al-mariiii55/IO/OutputDeviceh.h
@@ -0,0 +1,26 @@
+#ifndef OUTPUTDEVICE_H
+#define OUTPUTDEVICE_H
+#include "Observer.h"
+#include "OutputProcessingUnit.h"
+#include "..\Data-Structures\vector.h"
+
+class OutputDevice : public Observer
+{
+public:
+	OutputDevice(OutputProcessingUnit* opu)
+		:
+		opu(opu),
+		processedData(nullptr)
+	{
+	}
+	virtual ~OutputDevice(){}
+
+	virtual void update() = 0;
+	virtual void print() = 0;
+
+protected:
+	Vector<int>* processedData;
+	OutputProcessingUnit* opu;
+};
+#endif // !OUTPUTDEVICE_H
+
